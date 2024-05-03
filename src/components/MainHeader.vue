dc8b34076c51a558f1a22e35b024dfefea79dd78
ulisescastell <ulises.castell.7e7@itb.cat>
2024-05-03 13:57:20 +0200

    create header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <img src="/img/logoITBFLIX.png" alt="logotipo itb"/>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Peliculas</a>
+            <a href="#">Series</a>
+            <a href="#">Lo más visto</a>
+            <a href="#">Regístrate</a>
+        </nav>
+    </header>
+</template>
+
+
+<script>
+    export default {
+    name: 'MainHeader'
+    }
+</script>
+
+
+<style>
+
+</style>
